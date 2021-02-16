7d589c081cbd0aed2f2439cdbd5ac37e0e31055b
YuraKis <yurkis02@gmail.com>
2021-02-16 09:23:08 +0200

    init projects

---
+++ Met_Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Met_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
